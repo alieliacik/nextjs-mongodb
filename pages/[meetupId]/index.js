d726ca51766625d3f5cfc870c5bee912f48b3831
alieliacik <ali.eliacik@hotmail.com>
2023-01-01 20:39:57 -0500

    cnahge fallback

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -20,7 +20,7 @@ export async function getStaticPaths() {
   client.close()
   return {
     paths: meetups.map(item => ({ params: { meetupId: item._id.toString() } })),
-    fallback: false
+    fallback: 'blocking'
   }
 }
 
